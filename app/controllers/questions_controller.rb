e4fcb6bf2b0a97d6ed442739324c628ae2393828
smirnoff <smrnvmail@gmail.com>
2022-05-04 19:09:49 +0300

    after lesson 51

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,28 +1,28 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: %i[update show destroy edit hide]
-
   def create
     question = Question.create(question_params)
 
-    redirect_to question_path(question)
+    redirect_to question_path(question), notice: 'Новый вопрос создан!'
   end
 
   def update
     @question.update(question_params)
 
-    redirect_to question_path(@question)
+    redirect_to question_path(@question), notice: 'Сохранили вопрос!'
   end
 
   def destroy
     @question.destroy
 
-    redirect_to questions_path
+    redirect_to questions_path, notice: 'Вопрос удалён!'
   end
 
   def show
   end
 
-    def index
+  def index
+    @question = Question.new
     @questions = Question.all
   end
 
@@ -36,7 +36,7 @@ class QuestionsController < ApplicationController
   def hide
     @question.update(hidden: true)
 
-    redirect_to question_path(@question)
+    redirect_to question_path(@question), notice: 'Вопрос скрыт!'
   end
 
   private
