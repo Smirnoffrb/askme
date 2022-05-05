94fa2a560f87cc35cf9769cb6abc7cb7a3dc0b90
smirnoff <smrnvmail@gmail.com>
2022-05-05 14:28:53 +0300

    after lesson 52

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
+
+  resources :users, only: %i[new create]
+  resource :session, only: %i[new create destroy]
   resources :questions do
     patch :hide, on: :member
   end
