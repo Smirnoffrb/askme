e49d897014c187bdc0aff31844ac4016ef337851
smirnoff <smrnvmail@gmail.com>
2022-05-06 17:04:52 +0300

    user validation 53-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
 
+  VALID_EMAIL_REGEXP = /\A\w+@\w+\.[a-z]+/
+  VALID_NICKNAME_REGEXP = /\A[a-zA-Z_1-9]+\z/
+
+
   before_save :downcase_nickname
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEXP }
+
+  validates :nickname, uniqueness: true, length: { maximum: 40 }, format: { with: VALID_NICKNAME_REGEXP }
 
   def downcase_nickname
     nickname.downcase!
