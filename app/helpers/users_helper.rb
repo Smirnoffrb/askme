88dc153ac91f5437f151aeafb8eb2a1298b4f7ea
smirnoff <smrnvmail@gmail.com>
2022-05-09 21:34:58 +0300

    header color selection 53-3

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def display_nickname(user)
+    "@#{user.nickname}"
+  end
+
+  def header_color(user)
+    "#{user.header_color}"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,15 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEXP = /\A\w+@\w+\.[a-z]+/
   VALID_NICKNAME_REGEXP = /\A[a-zA-Z_1-9]+\z/
-
+  VALID_HEADER_REGEXP = /\A#[a-f0-9]{6}\z/i
 
   before_save :downcase_nickname
 
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEXP }
 
   validates :nickname, uniqueness: true, length: { maximum: 40 }, format: { with: VALID_NICKNAME_REGEXP }
+
+  validates :header_color, format: {with: VALID_HEADER_REGEXP}
 
   def downcase_nickname
     nickname.downcase!
