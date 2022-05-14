c35771d83ac001f3ea7e01cdb31c056e6a5766aa
smirnoff <smrnvmail@gmail.com>
2022-05-14 19:19:05 +0300

    add authors for questions backend 54-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
-  has_secure_password
 
   VALID_EMAIL_REGEXP = /\A\w+@\w+\.[a-z]+\z/
   VALID_NICKNAME_REGEXP = /\A[a-zA-Z_0-9]+\z/
   VALID_HEADER_REGEXP = /\A#\h{3}{1,2}\z/
+
+  has_many :questions, dependent: :delete_all
+  has_many :authored_question, class_name: 'Question', foreign_key: 'author_id', dependent: :nullify
+
+  has_secure_password
 
   before_validation :downcase_nickname
 
@@ -12,8 +16,6 @@ class User < ApplicationRecord
   validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: VALID_NICKNAME_REGEXP }
 
   validates :header_color, format: { with: VALID_HEADER_REGEXP }
-
-  has_many :questions, dependent: :delete_all
 
   private
 
